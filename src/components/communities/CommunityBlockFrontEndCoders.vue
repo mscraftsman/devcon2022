f6d5ceb4db298c63593cc1835a7917773ced4185
Sandeep Ramgolam <sandeep@ramgolam.com>
2021-11-28 22:45:08 +0400

    Update CommunityBlockFrontEndCoders.vue

--- src/components/communities/CommunityBlockFrontEndCoders.vue
+++ src/components/communities/CommunityBlockFrontEndCoders.vue
@@ -3,37 +3,30 @@
 		<div class="community__description">
 			<h2 class="community__title">Front-end Coders - Mauritius</h2>
 			<div class="community__info">
-				<p>
-					To be completed.
-				</p>
+				<p>Frontend Coders Mauritius is a group of mostly young adults who get together as often as possible either online or during meetups, with the goal of learning and discussing programming and exciting new topics revolving around frontend development. </p> <p>Members of the group come forward with presentations to share with the community making coding more accessible. It is a great place to socialize and as well as expand your career opportunities. </p>
+
+<p> Even though the past year has been a tough one, Front-end development has undergone stellar progress across all fronts and there's so much more to learn and share </p>
 
 				<div class="social__block">
 					<div class="website__info">
-						<a target="_blank" href="http://frontenddevmauritius.com/">
+						<a target="_blank" href="https://www.facebook.com/frontendcodersmauritius">
 							Website
 						</a>
 					</div>
 					<div class="social__icons">
-
-						<a href="https://twitter.com/FrontEndCoders" 
-							target="_blank" 
-							class="social__icon"
-						>
-							<img src="/icon/dark/twitter.svg" alt="GDG on Twitter" />
-						</a>
 						<a
 							href="https://www.facebook.com/frontendcodersmauritius"
 							class="social__icon"
 							target="_blank"
 						>
-							<img src="/icon/dark/facebook.svg" alt="GDG on Facebook" />
+							<img src="/icon/dark/facebook.svg" alt="Front-end Coders on Facebook" />
 						</a>
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="community__logo">
-			<!-- <img src="/images/communities/gdg.svg" alt=""> -->
+			<!-- <img src="/images/communities/front-end-coders.png" alt=""> -->
 		</div>
 	</div>
 </template>
