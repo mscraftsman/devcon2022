3442c1194c2319cb44423e579c559bda6f0e9b8d
Ish Sookun <ish.sookun@lasentinelle.mu>
2022-09-11 18:38:30 +0400

    Updated intro text

--- src/components/home/TextComponent.vue
+++ src/components/home/TextComponent.vue
@@ -18,10 +18,7 @@
             Being the largest IT Community in Mauritius, we are so passionate
             about the tech world that doing the Developers Conference is a must.
             With the Virtual Conference, we missed the human touch where people
-            exchange and interact with each other. Nonetheless, your safety is
-            our main priority and we will be putting in place some precautionary
-            measures, following our government's recommendations, to ensure that
-            our conference can be held safely.
+            exchange and interact with each other.
           </p>
           <p>
             Mark your calendar, save the dates and join us for the Developers
@@ -29,7 +26,7 @@
             technical geekery as well as various evening entertainments. Come
             and meet our Partners for recruitment and internship opportunities
             at their different booths. We have some surprises also in store for
-            you! So, come, and let’s have some fun!
+            you! So, come, and let's have some fun!
           </p>
         </div>
       </div>
